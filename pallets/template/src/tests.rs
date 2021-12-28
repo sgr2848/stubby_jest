2381fe4263c6d94a2fbc91fec750b58950bd6f3b
sagar <sg28r8@gmail.com>
2021-12-28 13:40:10 +0545

    1st commit for test cases

--- pallets/template/src/tests.rs
+++ pallets/template/src/tests.rs
@@ -1,6 +1,6 @@
-use frame_support::{assert_noop,assert_ok}
+// use frame_support::{assert_noop,assert_ok}
 
-#[test]
-fn it_works_for_signature() {
-    new_test_ext().execute_with(||)
-}
+// #[test]
+// fn it_works_for_signature() {
+//     new_test_ext().execute_with(||)
+// }
